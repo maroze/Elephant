a41829faa1226547189ad9eaa7198cbf94bf5478
maroze <91451262+maroze@users.noreply.github.com>
2022-12-22 18:02:59 +0300

    Add files via upload

---
+++ Elephant/Elephant/Elephants.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Elephant
+{
+    class Elephants
+    {
+        public string Name;
+        public double EarSize;
+
+        /// <summary>
+        /// Информация о слоне
+        /// </summary>
+        /// <returns>Возвращает имя и размер ушей слона</returns>
+        public string WhoAmI()
+        {
+            return "Моё имя " + Name + "\n" + "Размер моих ушей " + EarSize + " см";
+        }
+
+        /// <summary>
+        /// Метод для прослушивания сообщения
+        /// </summary>
+        /// <param name="message">текст сообщения</param>
+        /// <param name="whoSaid">отправитель сообщения</param>
+        /// <returns>в первой части адресат прослушал сообщение, во второй
+        /// имя отправителя и содержание сообщения</returns>
+        public void HearMessage(string message, Elephants whoSaidIt)
+        {
+            Console.WriteLine( Name + " прослушал сообщение\n" + whoSaidIt.Name + " отправил это сообщение: " + message);
+        }
+
+        /// <summary>
+        /// Метод для отправки сообщения
+        /// </summary>
+        /// <param name="whoToTalkTo">отправитель</param>
+        /// <param name="message">текст сообщения</param>
+        public void SpeakTo(Elephants whoToTalkTo, string message)
+        {
+            whoToTalkTo.HearMessage(message, this);
+        }
+    }
+}
---
+++ Elephant/Elephant/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+
+namespace Elephant
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Elephants two = new Elephants() { Name = "Lucinda", EarSize = 33 };
+            Elephants one = new Elephants() { Name = "Lloyd", EarSize = 40 };
+
+            Console.WriteLine("Нажмите 1 для Lloyd, 2 для Lucinda, 3 поменять ссылки");
+
+            while (true)
+            {
+                char input = Console.ReadKey(true).KeyChar;
+                Console.WriteLine("Вы нажали: " + input);
+
+                if (input == '1')
+                {
+                    Console.WriteLine("Вызываем lloyd.WhoAmI()\n" + one.WhoAmI());
+                }
+
+                else if (input == '2')
+                {
+                    Console.WriteLine("Вызываем lucinda.WhoAmI()\n" + two.WhoAmI());
+                }
+
+                else if (input == '3')
+                {
+                    Elephants three;
+                    three = two;
+                    two = one;
+                    one = three;
+
+                    Console.WriteLine("Ссылки были заменены местами");
+                }
+
+                else if (input == '4')
+                {
+                    one = two;
+                    one.EarSize = 4321;
+                    Console.WriteLine(two.WhoAmI());
+                }
+
+                else if (input == '5')
+                {
+                    two.SpeakTo(one, "Hi, Lloyd!");
+                }
+
+                else
+                {
+                    return;
+                }
+            }
+        }
+
+    }
+}
+
